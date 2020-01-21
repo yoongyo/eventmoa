62a566c027e862100507d788f9bf62cbc77fb33c
yoongyo <jyg0172@naver.com>
2020-01-22 07:36:39 +0900

    AD

--- event/schema.py
+++ event/schema.py
@@ -18,8 +18,30 @@ class Query(graphene.AbstractType):
 
     all_partner = graphene.List(PartnerType)
 
+    event = graphene.Field(EventType,
+                              id=graphene.Int())
+
+    partner = graphene.Field(PartnerType,
+                             id=graphene.Int())
+
     def resolve_all_event(self, context, **kwargs):
         return Event.objects.all()
 
+    def resolve_event(self, context, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Event.objects.get(pk=id)
+        return None
+
     def resolve_all_partner(self, context, **kwargs):
         return Partner.objects.all()
+
+    def resolve_partner(self, context, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Partner.objects.get(pk=id)
+        return None
+
+
