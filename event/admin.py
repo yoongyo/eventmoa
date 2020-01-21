dbf97ea17ab58a82ff30556c057b1a9e5aacf7fa
yoongyo <jyg0172@naver.com>
2020-01-21 15:54:07 +0900

    AD

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Event, HashTag, Partner
 
-# Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'partner']
+
+
+class PartnerAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+class HashTagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+admin.site.register(HashTagAdmin, HashTag)
+admin.site.register(EventAdmin, Event)
+admin.site.regitser(PartnerAdmin, Partner)
